c1bf0cd72b0ea9d8e3a0968eec0460528497ae43
kmsmahi <salauddinmahi2002@gmail.com>
2026-09-04 16:41:13 +0600

    initial page and componenets setup

--- src/Routs/AllRouts.jsx
+++ src/Routs/AllRouts.jsx
@@ -1,8 +1,36 @@
 import { createBrowserRouter } from "react-router";
+import MainLayout from "../LayOuts/MainLayout";
+import Home from "../Pages/Home";
+import AllProducts from "../Pages/AllProducts";
+import MyProducts from "../Pages/MyProducts";
+import MyBids from "../Pages/MyBids";
+import CreateProduct from "../Pages/CreateProduct";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <button className="btn btn-primary">Primary</button>,
+    element: <MainLayout></MainLayout>,
+    children:[
+      {
+        index:true,
+        element:<Home></Home>
+      },
+      {
+        path:'/all-products',
+        element:<AllProducts></AllProducts>
+      },
+      {
+        path:'/my-products',
+        element:<MyProducts></MyProducts>
+      },
+      {
+        path:'/my-bids',
+        element:<MyBids></MyBids>
+      },
+      {
+        path:'/create-product',
+        element:<CreateProduct></CreateProduct>
+      },
+    ]
   },
 ]);
 export default router;
