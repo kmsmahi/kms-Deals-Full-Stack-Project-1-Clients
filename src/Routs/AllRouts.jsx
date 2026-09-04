49a42ec81874c4862c1e6d86a680909a7679c51b
kmsmahi <salauddinmahi2002@gmail.com>
2026-09-04 10:31:16 +0600

    initial setup

---
+++ src/Routs/AllRouts.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <button className="btn btn-primary">Primary</button>,
+  },
+]);
+export default router;
